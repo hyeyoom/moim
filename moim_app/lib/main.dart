f813da4af2185c60be4276a07e557cc22330cec4
hyeyoom <neoul_chw@icloud.com>
2022-02-18 01:57:55 +0900

    flutter practice

---
+++ moim_app/lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:moim_app/design_system.dart';
+import 'package:moim_app/screens/welcome/welcome_screen.dart';
+
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Moim',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primaryColor: primaryColor,
+        scaffoldBackgroundColor: Colors.white,
+      ),
+      home: const WelcomeScreen(),
+    );
+  }
+}
---
+++ moim_app/lib/screens/welcome/weclome_body.dart
@@ -0,0 +1,67 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:moim_app/design_system.dart';
+
+import 'welcome_background.dart';
+import '../../components/buttons.dart';
+import '../login/login_screen.dart';
+
+class WelcomeBody extends StatelessWidget {
+  const WelcomeBody({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
+    return WelcomeBackground(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          const Align(
+            alignment: AlignmentDirectional.center,
+            child: Text(
+              'Greeting from Moim',
+              style: TextStyle(fontWeight: FontWeight.bold),
+            ),
+          ),
+          SizedBox(height: size.height * 0.03,),
+          Align(
+            alignment: AlignmentDirectional.center,
+            child: SvgPicture.asset(
+              'assets/icons/chat.svg',
+              height: size.height * 0.45,
+            ),
+          ),
+          SizedBox(height: size.height * 0.03,),
+          Align(
+            alignment: AlignmentDirectional.center,
+            child: RoundedButton(
+              text: 'LOGIN',
+              onPress: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) {
+                      return LoginScreen();
+                    }
+                  ),
+                );
+              },
+              color: primaryColor,
+              textColor: Colors.white,
+            ),
+          ),
+          Align(
+            alignment: AlignmentDirectional.center,
+            child: RoundedButton(
+              text: 'SIGN UP',
+              onPress: () {},
+              color: primaryWeakColor,
+              textColor: Colors.black,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
