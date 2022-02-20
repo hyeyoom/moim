9a9972d5065a78c996ef35f1d47f1c0b0071c008
hyeyoom <neoul_chw@icloud.com>
2022-02-20 22:28:14 +0900

    added mock routing structure for testing user flow

--- moim_app/lib/main.dart
+++ moim_app/lib/main.dart
@@ -1,13 +1,35 @@
 import 'package:flutter/material.dart';
-import 'package:moim_app/core/constants/design_system.dart';
+import 'package:moim_app/core/storage/local_storage.dart';
 import 'package:moim_app/screens/intro/welcome/welcome_screen.dart';
+import 'package:moim_app/screens/main/MainScreen.dart';
 
-void main() => runApp(const MyApp());
+import 'core/constants/design_system.dart';
+import 'core/storage/shared_preference_local_storage.dart';
 
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+void main() => runApp(const MoimApp());
 
-  // This widget is the root of your application.
+class MoimApp extends StatefulWidget {
+  const MoimApp({Key? key}) : super(key: key);
+
+  @override
+  _MoimAppState createState() => _MoimAppState();
+}
+
+class _MoimAppState extends State<MoimApp> {
+  LocalStorage localStorage = SharedPreferenceLocalStorage();
+
+  @override
+  void initState() {
+    localStorage.init();
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    localStorage.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -17,7 +39,39 @@ class MyApp extends StatelessWidget {
         primaryColor: primaryColor,
         scaffoldBackgroundColor: Colors.white,
       ),
-      home: const WelcomeScreen(),
+      home: determineScreen(),
     );
   }
+
+  // FIXME: 분명 라우트하는 방식이 있을거임.
+  Widget determineScreen() {
+    String? refreshKey = localStorage.load('refresh_key');
+    if (refreshKey == null) {
+      return const WelcomeScreen();
+    }
+    return const MainScreen();
+  }
 }
+
+/*
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    */
+/*return MaterialApp(
+      title: 'Moim',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primaryColor: primaryColor,
+        scaffoldBackgroundColor: Colors.white,
+      ),
+      home: const WelcomeScreen(),
+    );*/ /*
+
+    return const WelcomeScreen();
+  }
+}
+*/
---
+++ moim_app/lib/screens/main/MainScreen.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+
+class MainScreen extends StatelessWidget {
+  const MainScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Hello'),
+      ),
+      body: Container(
+        color: Colors.blue,
+      ),
+      bottomNavigationBar: BottomAppBar(
+        child: SizedBox(
+          height: 70,
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: const <Widget>[
+              Icon(Icons.phone),
+              Icon(Icons.message),
+              Icon(Icons.contact_page),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
